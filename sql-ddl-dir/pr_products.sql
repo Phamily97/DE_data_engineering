64e8ef06a6b1e22d055a6e387209bdd087805286
Van <phamilyvan97@gmail.com>
2025-02-22 23:27:22 +1100

    Added in computed columns for pr_procucts

--- sql-ddl-dir/pr_products.sql
+++ sql-ddl-dir/pr_products.sql
@@ -12,3 +12,30 @@ CREATE TABLE pr_products (
 	,country_id INT NULL
 	,Last_updated_UTC DATETIME2(7) NOT NULL CONSTRAINT DF_pr_products_Last_updated_UTC DEFAULT(GETDATE()) 
 )
+/*Ticket: DP-7*/
+ALTER TABLE pr_products ADD Status INT NOT NULL CONSTRAINT DF_pr_products_Status DEFAULT(0)
+GO
+
+ALTER TABLE pr_products ADD Status_computed AS (
+	CASE WHEN status = 0 THEN 'Active'
+		WHEN status = 1 THEN 'On-Hold'
+		WHEN status = 2 THEN 'Inactive'
+		WHEN status = 3 THEN 'Archive'
+	END
+) PERSISTED
+GO
+
+ALTER TABLE pr_products ADD type_computed AS (
+	CASE WHEN type_id = 0 THEN 'Food & Drinks' 
+		WHEN type_id = 1 THEN 'Clothing'
+		WHEN type_id = 2 THEN 'Electronics'
+		WHEN type_id = 3 THEN 'Equipment'
+		WHEN type_id = 4 THEN 'Vehicle & Parts'
+		WHEN type_id = 5 THEN 'Cosmetics'
+		WHEN type_id = 6 THEN 'Everyday items'
+		WHEN type_id = 7 THEN 'Gaming'
+		WHEN type_id = 8 THEN 'Home Appliance'
+		WHEN type_id = 9 THEN 'Music'
+	END
+) PERSISTED
+GO
