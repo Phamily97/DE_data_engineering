f97f622d94f44893a6c07cd26b25cbf2d8e1a2d5
Van <phamilyvan97@gmail.com>
2025-02-24 19:52:16 +1100

    Added in update trigger, operation key to json

--- sql-ddl-trigger/tr_event_pr_products.sql
+++ sql-ddl-trigger/tr_event_pr_products.sql
@@ -4,9 +4,6 @@ AS
 BEGIN
 	DECLARE @operation TINYINT = 0
 
-	SELECT COUNT(1) FROM inserted
-	SELECT COUNT(1) FROM deleted
-
 	SET @operation = 
 		CASE
 			--INSERTED operation
@@ -34,7 +31,7 @@ BEGIN
 			,Status AS Status
 			,Last_updated_UTC AS Last_updated_UTC
 		FROM INSERTED i
-		FOR JSON PATH) json_obj) json_obj
+		FOR JSON PATH, ROOT('ins')) json_obj) json_obj
 	END
 	
 	IF @operation = 2
@@ -52,7 +49,36 @@ BEGIN
 			,Status AS Status
 			,Last_updated_UTC AS Last_updated_UTC
 		FROM DELETED d
-		FOR JSON PATH) json_obj) json_obj
+		FOR JSON PATH, ROOT('del')) json_obj) json_obj
 	END
 
+	IF @operation = 3
+	BEGIN
+		INSERT INTO event_log_pr_products(event, event_type)
+			SELECT json_obj.json_obj
+				,@operation
+			FROM (SELECT (
+			SELECT NULLIF(i.Product, d.Product) AS Product
+				,NULLIF(i.OrderNo, d.OrderNo) AS OrderNo
+				,NULLIF(i.Qty, d.Qty) AS Qty
+				,NULLIF(i.CostEx, d.CostEx) AS CostEx
+				,NULLIF(i.SellEx, d.SellEx) AS SellEx
+				,NULLIF(i.Tax, d.Tax) AS Tax
+				,NULLIF(i.expiration_date, d.expiration_date) AS expiration_date
+				,NULLIF(i.Status, d.Status) AS Status
+				,NULLIF(i.Last_updated_UTC, d.Last_updated_UTC) AS Last_updated_UTC
+			FROM INSERTED i
+			OUTER APPLY (SELECT Product AS Product
+				,OrderNo AS OrderNo
+				,Qty AS Qty
+				,CostEx AS CostEx
+				,SellEx AS SellEx
+				,Tax AS Tax
+				,expiration_date AS expiration_date
+				,Status AS Status
+				,Last_updated_UTC AS Last_updated_UTC
+			FROM DELETED d) d
+			FOR JSON PATH, ROOT('upd')
+			) json_obj) json_obj
+	END
 END
